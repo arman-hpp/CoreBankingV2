ce189b560bb9bf4456c221b1177a3670c645b5d4
arman-hpp <arman.hassanpoor2000@gmail.com>
2024-08-06 20:52:09 +0330

    update

--- src/main/java/com/bank/enums/ledgers/LedgerNatures.java
+++ src/main/java/com/bank/enums/ledgers/LedgerNatures.java
@@ -1,4 +1,8 @@
 package com.bank.enums.ledgers;
 
 public enum LedgerNatures {
+    Unknown,
+    Both,
+    Debtor,
+    Creditor
 }
--- src/main/java/com/bank/enums/ledgers/LedgerTypes.java
+++ src/main/java/com/bank/enums/ledgers/LedgerTypes.java
@@ -1,4 +1,8 @@
 package com.bank.enums.ledgers;
 
 public enum LedgerTypes {
+    Unknown,
+    Permanent,
+    Temporary,
+    Control
 }
