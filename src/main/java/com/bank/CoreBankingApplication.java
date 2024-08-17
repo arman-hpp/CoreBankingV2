99d24cea9855e26b5ca712ce000c494dc0f86a20
Arman <arman.hassanpoor2000@gmail.com>
2024-08-17 12:49:24 +0330

    #2014-08-17

--- src/main/java/com/bank/CoreBankingApplication.java
+++ src/main/java/com/bank/CoreBankingApplication.java
@@ -11,9 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {"com.bank.models.*"})
 @EnableCaching
 public class CoreBankingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CoreBankingApplication.class, args);
     }
-
 }
