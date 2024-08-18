2b03864097f53f6fb10619c368560abfb5750326
Arman <arman.hassanpoor2000@gmail.com>
2024-08-18 09:30:48 +0330

    #2014-08-18

--- src/main/java/com/bank/services/users/UserService.java
+++ src/main/java/com/bank/services/users/UserService.java
@@ -2,14 +2,11 @@ package com.bank.services.users;
 
 import com.bank.dtos.PagedResponseDto;
 import com.bank.dtos.users.UserDto;
-import com.bank.enums.users.UserTypes;
 import com.bank.exceptions.DomainException;
 import com.bank.repos.users.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @SuppressWarnings("unused")
@@ -21,15 +18,6 @@ public class UserService  {
     public UserService(UserRepository userRepository, ModelMapper modelMapper) {
         _userRepository = userRepository;
         _modelMapper = modelMapper;
-    }
-
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var user = _userRepository.findByUsername(username).orElse(null);
-        if (user == null) {
-            throw new UsernameNotFoundException("The user cannot be found");
-        }
-
-        return user;
     }
 
     public UserDto loadUser(Long userId) {
@@ -64,13 +52,5 @@ public class UserService  {
         _userRepository.save(user);
 
         return userDto;
-    }
-
-    public Boolean isAdmin(Long userId) {
-        var user = _userRepository.findById(userId).orElse(null);
-        if(user == null)
-            throw new DomainException("error.auth.notFound");
-
-        return user.getUserType() == UserTypes.ROLE_ADMIN;
     }
 }
