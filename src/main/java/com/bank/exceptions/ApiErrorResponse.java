451e0c8350df2889e7121cc996842a58c15f504d
Arman <arman.hassanpoor2000@gmail.com>
2025-04-29 09:21:56 +0330

    convert class to record

--- src/main/java/com/bank/exceptions/ApiErrorResponse.java
+++ src/main/java/com/bank/exceptions/ApiErrorResponse.java
@@ -5,16 +5,5 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 
 @Getter
-public class ApiErrorResponse {
-    private final String error;
-    private final String message;
-    private final int status;
-    private final LocalDateTime timestamp;
-
-    public ApiErrorResponse(String error, String message, int status, LocalDateTime timestamp) {
-        this.error = error;
-        this.message = message;
-        this.status = status;
-        this.timestamp = timestamp;
-    }
+public record ApiErrorResponse(String error, String message, int status, LocalDateTime timestamp) {
 }
