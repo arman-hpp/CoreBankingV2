0605962de0bbe0fcf0147ac19ea87f14f04ceb5f
Arman <arman.hassanpoor2000@gmail.com>
2025-04-30 06:59:31 +0330

    fix record error

--- src/main/java/com/bank/exceptions/ApiErrorResponse.java
+++ src/main/java/com/bank/exceptions/ApiErrorResponse.java
@@ -1,9 +1,7 @@
 package com.bank.exceptions;
 
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
 public record ApiErrorResponse(String error, String message, int status, LocalDateTime timestamp) {
 }
