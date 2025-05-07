65f9e5f29a600118cf4e4a05eea512a4d16dd751
Arman <arman.hassanpoor2000@gmail.com>
2025-05-07 12:59:07 +0330

    refactor services and controllers

--- src/main/java/com/bank/customers/controllers/CustomerController.java
+++ src/main/java/com/bank/customers/controllers/CustomerController.java
@@ -5,47 +5,51 @@ import com.bank.core.dtos.filters.FilterInfoDto;
 import com.bank.customers.dtos.AddCustomerRequestDto;
 import com.bank.customers.dtos.CustomerResponseDto;
 import com.bank.customers.dtos.EditCustomerRequestDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import com.bank.customers.services.CustomerService;
 
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
-    private final CustomerService _customerService;
+    private final CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {
-        _customerService = customerService;
+        this.customerService = customerService;
     }
 
-    @GetMapping("/")
+    @GetMapping
     public PagedResponseDto<CustomerResponseDto> getAllCustomers(
             @RequestParam(defaultValue = "0") Integer page,
             @RequestParam(defaultValue = "10") Integer size) {
-        return _customerService.loadCustomers(page, size);
+        return customerService.loadCustomers(page, size);
     }
 
     @GetMapping("/{id}")
     public CustomerResponseDto getCustomerById(@PathVariable Long id) {
-        return _customerService.loadCustomer(id);
+        return customerService.loadCustomer(id);
     }
 
-    @PostMapping("/")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public CustomerResponseDto addCustomer(@RequestBody AddCustomerRequestDto customerDto) {
-        return _customerService.addCustomer(customerDto);
+        return customerService.addCustomer(customerDto);
     }
 
-    @PostMapping("/")
-    public CustomerResponseDto editCustomer(@RequestBody EditCustomerRequestDto customerDto) {
-        return _customerService.editCustomer(customerDto);
+    @PutMapping("/{id}")
+    public CustomerResponseDto editCustomer(@PathVariable Long id, @RequestBody EditCustomerRequestDto customerDto) {
+        customerDto.setId(id);
+        return customerService.editCustomer(customerDto);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCustomer(@PathVariable Long id) {
-        _customerService.removeCustomer(id);
+        customerService.removeCustomer(id);
     }
 
     @PostMapping("/filter")
     public PagedResponseDto<CustomerResponseDto> filterCustomers(@RequestBody FilterInfoDto filterInfo) {
-        return _customerService.loadCustomerByFilter(filterInfo);
+        return customerService.loadCustomerByFilter(filterInfo);
     }
 }
