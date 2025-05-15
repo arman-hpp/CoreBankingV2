962056b5ce94cf1217cc682eee8fcd14db66fbd9
arman-hpp <arman.hassanpoor2000@gmail.com>
2025-05-15 09:40:27 +0330

    add dev and prod env

--- src/main/java/com/bank/core/configs/WebSecurityConfiguration.java
+++ src/main/java/com/bank/core/configs/WebSecurityConfiguration.java
@@ -4,6 +4,8 @@ import com.bank.core.configs.filters.JwtAuthenticationFilter;
 import com.bank.core.configs.filters.RateLimitingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,13 +22,16 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration {
-    public final JwtAuthenticationFilter _jwtAuthenticationFilter;
-    public final RateLimitingFilter _rateLimitingFilter;
+    public final JwtAuthenticationFilter jwtAuthenticationFilter;
+    public final RateLimitingFilter rateLimitingFilter;
+    private final Environment environment;
 
-    public WebSecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter,
+    public WebSecurityConfiguration(Environment environment,
+                                    JwtAuthenticationFilter jwtAuthenticationFilter,
                                     RateLimitingFilter rateLimitingFilter) {
-        _jwtAuthenticationFilter = jwtAuthenticationFilter;
-        _rateLimitingFilter = rateLimitingFilter;
+        this.environment = environment;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.rateLimitingFilter = rateLimitingFilter;
     }
 
     @Bean
@@ -34,27 +39,44 @@ public class WebSecurityConfiguration {
         http
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/h2/**").permitAll()
-                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/api/test/**").permitAll()
-                        .requestMatchers("/error/**").permitAll()
-                        //.requestMatchers("/api/v1/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/auth/captcha").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/auth/login", "/api/auth/register", "/api/auth/refresh_token").permitAll()
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .anyRequest().authenticated()
-                );
+                .authorizeHttpRequests((requests) -> {
+                    if (environment.acceptsProfiles(Profiles.of("prod"))) {
+                        requests
+                                .requestMatchers("/error/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/auth/captcha").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/auth/login", "/api/auth/register", "/api/auth/refresh_token").permitAll()
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+                    }
+                    else {
+                        requests
+                                .requestMatchers("/h2/**").permitAll()
+                                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .requestMatchers("/api/test/**").permitAll()
+                                .requestMatchers("/error/**").permitAll()
+                                .requestMatchers("/api/dev/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/auth/captcha").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/auth/login", "/api/auth/register", "/api/auth/refresh_token").permitAll()
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+                    }
 
-        http.addFilterBefore(_rateLimitingFilter, UsernamePasswordAuthenticationFilter.class);
-        http.addFilterAfter(_jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                    requests.anyRequest().authenticated();
+                });
+        if (environment.acceptsProfiles(Profiles.of("prod"))) {
+            http.addFilterBefore(rateLimitingFilter, UsernamePasswordAuthenticationFilter.class);
+        }
+        http.addFilterAfter(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         var configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8090", "http://127.0.0.1:8090"));
+        if (environment.acceptsProfiles(Profiles.of("prod"))) {
+            configuration.setAllowedOrigins(List.of("http://192.168.1.102:8091"));
+        } else {
+            configuration.setAllowedOrigins(List.of("http://localhost:8090", "http://127.0.0.1:8090"));
+        }
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
